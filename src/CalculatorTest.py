1516c6c69ed40d3b8de5cac2db78b69ea6776f61
hindl <hu23@njit.edu>
2019-11-01 02:37:35 -0400

    task:setup calculator with subtraction

--- src/CalculatorTest.py
+++ src/CalculatorTest.py
@@ -1,6 +1,7 @@
 import unittest
 from Calculator import Calculator
 
+
 class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         calculator = Calculator()
@@ -8,13 +9,18 @@ class MyTestCase(unittest.TestCase):
 
     def test_result_property_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(calculator.result, 0)
 
     def test_add_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.add(2,2), 4)
+        self.assertEqual(calculator.result, 4)
 
+    def test_subtract_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(2, 2), 0)
+        self.assertEqual(calculator.result, 0)
 
-   if __name__ == '__main__':
+if __name__ == '__main__' :
        unittest.main()
 
